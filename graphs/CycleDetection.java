b120fef951df35fd43aa678db5390e49602ea37a
Allen Saji <allensaji04@gmail.com>
2023-04-24 07:56:23 +0530

    add cycle detection problem for directed graphs

--- graphs/CycleDetection.java
+++ graphs/CycleDetection.java
@@ -73,6 +73,38 @@ public class CycleDetection {
         return true;
     }
 
+    // for directed graph
+    public static boolean isCycle(ArrayList<Edge> graph[]) {
+        boolean vis[] = new boolean[graph.length];
+        boolean stack[] = new boolean[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if (isCycleUtil(graph, i, vis, stack)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isCycleUtil(ArrayList<Edge> graph[], int curr, boolean[] vis, boolean[] stack) {
+        stack[curr] = true;
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (stack[e.dest]) {
+                return true; // cycle exist
+            }
+            if (!vis[e.dest] && isCycleUtil(graph, e.dest, vis, stack)) {
+                return true;
+            }
+        }
+        stack[curr] = false;
+        return false;
+    }
+
     public static void main(String[] args) {
         int V = 5; // no. of vertices of graph
         ArrayList<Edge>[] graph = new ArrayList[V]; // array of arraylist
@@ -80,27 +112,34 @@ public class CycleDetection {
             graph[i] = new ArrayList<>();
         }
 
-        // 0th vertex
-        graph[0].add(new Edge(0, 1));
+        // // 0th vertex
+        // graph[0].add(new Edge(0, 1));
 
-        // 1st vertex
+        // // 1st vertex
+        // graph[1].add(new Edge(1, 0));
+        // graph[1].add(new Edge(1, 2));
+        // graph[1].add(new Edge(1, 3));
+
+        // // 2nd vertex
+        // graph[2].add(new Edge(2, 1));
+        // graph[2].add(new Edge(2, 4));
+        // graph[2].add(new Edge(2, 3));
+
+        // // 3rd vertex
+        // graph[3].add(new Edge(3, 1));
+        // graph[3].add(new Edge(3, 2));
+
+        // // 4th vertex
+        // graph[4].add(new Edge(4, 2));
+
+        // directed graph
+        graph[0].add(new Edge(0, 2));
         graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 2));
-        graph[1].add(new Edge(1, 3));
-
-        // 2nd vertex
-        graph[2].add(new Edge(2, 1));
-        graph[2].add(new Edge(2, 4));
         graph[2].add(new Edge(2, 3));
-
-        // 3rd vertex
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 2));
-
-        // 4th vertex
-        graph[4].add(new Edge(4, 2));
+        // graph[3].add(new Edge(3, 0));
 
         // System.out.println(detectCycle(graph));
-        System.out.println(isBipartite(graph));
+        // System.out.println(isBipartite(graph));
+        System.out.println(isCycle(graph));
     }
 }
