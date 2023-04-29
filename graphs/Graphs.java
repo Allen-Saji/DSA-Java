90a99e061c4f519b2dce856b946b8d8b26903700
Allen Saji <allensaji04@gmail.com>
2023-04-29 20:00:28 +0530

    add prim's algorithm

--- graphs/Graphs.java
+++ graphs/Graphs.java
@@ -85,58 +85,124 @@ public class Graphs {
         return false;
     }
 
-    static class Pair implements Comparable<Pair> {
-        int n; // node
-        int path;
+    // static class Pair implements Comparable<Pair> {
+    // int n; // node
+    // int path;
 
-        public Pair(int n, int path) {
-            this.n = n;
-            this.path = path;
+    // public Pair(int n, int path) {
+    // this.n = n;
+    // this.path = path;
+    // }
+
+    // @Override
+    // public int compareTo(Pair p2) {
+    // return this.path - p2.path; // path based sorting for pairs
+    // }
+    // }
+
+    // tc: O(V+ElogV) because we used priority queue else O(V^2)
+    // ElogV PQ ke dist ki sorting ka time h
+    // public static void dijkstra(ArrayList<Edge> graph[], int src, int dest) {
+    // int dist[] = new int[graph.length]; // dist[i]: dist from src to i
+    // for (int i = 0; i < graph.length; i++) {
+    // if (i != src) {
+    // dist[i] = Integer.MAX_VALUE;
+    // }
+    // }
+
+    // boolean vis[] = new boolean[graph.length];
+    // PriorityQueue<Pair> pq = new PriorityQueue<>();
+    // pq.add(new Pair(src, 0));
+    // // loop
+    // while (!pq.isEmpty()) {
+    // Pair curr = pq.remove();
+    // if (!vis[curr.n]) {
+    // vis[curr.n] = true;
+    // }
+    // for (int i = 0; i < graph[curr.n].size(); i++) {
+    // Edge e = graph[curr.n].get(i);
+    // int u = e.src;
+    // int v = e.dest;
+    // int wt = e.dest;
+
+    // if (dist[u] + wt < dist[v]) {
+    // dist[v] = dist[u] + wt;
+    // pq.add(new Pair(v, dist[v]));
+    // }
+    // }
+    // }
+    // // print all src to dest shortest path
+    // for (int i = 0; i < graph.length; i++) {
+    // System.out.print(dist[i] + " ");
+    // }
+    // System.out.println();
+    // }
+
+    // // TC: O(V+E)
+    // public static void bellmanFord(ArrayList<Edge> graph[], int src) {
+    // int dist[] = new int[graph.length];
+    // for (int i = 0; i < graph.length; i++) {
+    // if (i != src) {
+    // dist[i] = Integer.MAX_VALUE;
+    // }
+    // }
+
+    // int V = graph.length;
+
+    // for (int i = 0; i < V - 1; i++) {
+    // // TC = no. of edges i.e. O(E)
+    // for (int j = 0; j < graph.length; j++) {
+    // for (int k = 0; k < graph[j].size(); k++) {
+    // Edge e = graph[j].get(k);
+    // int u = e.src;
+    // int v = e.dest;
+    // int wt = e.wt;
+
+    // if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
+    // dist[v] = dist[u] + wt;
+    // }
+    // }
+    // }
+    // }
+
+    // for (int i = 0; i < dist.length; i++) {
+    // System.out.print(dist[i] + " ");
+    // }
+    // }
+
+    static class Pair implements Comparable<Pair> {
+        int v, cost;
+
+        public Pair(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
         }
 
         @Override
         public int compareTo(Pair p2) {
-            return this.path - p2.path; // path based sorting for pairs
+            return this.cost - p2.cost; // ascending
         }
     }
 
-    // tc: O(V+ElogV) because we used priority queue else O(V^2)
-    // ElogV PQ ke dist ki sorting ka time h
-    public static void dijkstra(ArrayList<Edge> graph[], int src, int dest) {
-        int dist[] = new int[graph.length]; // dist[i]: dist from src to i
-        for (int i = 0; i < graph.length; i++) {
-            if (i != src) {
-                dist[i] = Integer.MAX_VALUE;
-            }
-        }
-
+    public static void prims(ArrayList<Edge> graph[]) {
         boolean vis[] = new boolean[graph.length];
         PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(src, 0));
-        // loop
+        pq.add(new Pair(0, 0));
+        int finalCost = 0;
+
         while (!pq.isEmpty()) {
             Pair curr = pq.remove();
-            if (!vis[curr.n]) {
-                vis[curr.n] = true;
-            }
-            for (int i = 0; i < graph[curr.n].size(); i++) {
-                Edge e = graph[curr.n].get(i);
-                int u = e.src;
-                int v = e.dest;
-                int wt = e.dest;
+            if (!vis[curr.v]) {
+                vis[curr.v] = true;
+                finalCost += curr.cost;
 
-                if (dist[u] + wt < dist[v]) {
-                    dist[v] = dist[u] + wt;
-                    pq.add(new Pair(v, dist[v]));
+                for (int i = 0; i < graph[curr.v].size(); i++) {
+                    Edge e = graph[curr.v].get(i);
+                    pq.add(new Pair(e.dest, e.wt));
                 }
             }
         }
-
-        // print all src to dest shortest path
-        for (int i = 0; i < graph.length; i++) {
-            System.out.print(dist[i] + " ");
-        }
-        System.out.println();
+        System.out.println("Cost of MST: " + finalCost);
     }
 
     public static void main(String[] args) {
@@ -147,24 +213,26 @@ public class Graphs {
         }
 
         // 0th vertex
-        graph[0].add(new Edge(0, 1, 5));
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
 
         // 1st vertex
-        graph[1].add(new Edge(1, 0, 5));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[1].add(new Edge(1, 3, 3));
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
+        // graph[1].add(new Edge(1, 3, 3));
 
         // 2nd vertex
-        graph[2].add(new Edge(2, 1, 1));
-        graph[2].add(new Edge(2, 4, 4));
-        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+        // graph[2].add(new Edge(2, 3, 1));
 
         // 3rd vertex
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
 
         // 4th vertex
-        graph[4].add(new Edge(4, 2, 4));
+        // graph[4].add(new Edge(4, 2, 4));
 
         // 2's neighbours
         // for (int i = 0; i < graph[2].size(); i++) {
@@ -175,6 +243,7 @@ public class Graphs {
         // bfs(graph);
         // dfs(graph, 0, new boolean[V]);
         // System.out.print(hasPath(graph, 0, 4, new boolean[V]));
+        prims(graph);
 
     }
 }
