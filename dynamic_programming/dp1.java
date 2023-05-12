ca7b07c4f9fa0d192072730ef1155ffca4d294b5
Allen Saji <allensaji04@gmail.com>
2023-05-12 09:10:02 +0530

    fibonacci memoization

---
+++ dynamic_programming/dp1.java
@@ -0,0 +1,23 @@
+package dynamic_programming;
+
+public class dp1 {
+
+    public static int fibonacci(int n, int[] f) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (f[n] != 0) {
+            return f[n];
+        }
+
+        f[n] = fibonacci(n - 1, f) + fibonacci(n - 2, f);
+        return f[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int f[] = new int[n + 1];
+        System.out.println(fibonacci(n, f));
+    }
+
+}
