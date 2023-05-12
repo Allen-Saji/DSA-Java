122cd2d7d02f305f28c5a8cb804ee3e33f1e6462
Allen Saji <allensaji04@gmail.com>
2023-05-12 15:34:17 +0530

    add fibonacci using tablution method

--- dynamic_programming/dp1.java
+++ dynamic_programming/dp1.java
@@ -14,10 +14,23 @@ public class dp1 {
         return f[n];
     }
 
+    public static int fibonacciTabulation(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
         int f[] = new int[n + 1];
         System.out.println(fibonacci(n, f));
+        System.out.println(fibonacciTabulation(n));
     }
 
 }
