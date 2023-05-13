6c505c583554cde4581d090467428c08cc8a1353
Allen Saji <allensaji04@gmail.com>
2023-05-13 08:15:20 +0530

    climbing stairs using memoization

--- dynamic_programming/dp1.java
+++ dynamic_programming/dp1.java
@@ -1,5 +1,7 @@
 package dynamic_programming;
 
+import java.util.*;
+
 public class dp1 {
 
     public static int fibonacci(int n, int[] f) {
@@ -26,11 +28,32 @@ public class dp1 {
         return dp[n];
     }
 
+    // climbing stairs problem
+    public static int countWays(int n, int ways[]) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 0;
+        }
+
+        if (ways[n] != -1) {
+            return ways[n];
+        }
+        ways[n] = countWays(n - 1, ways) + countWays(n - 2, ways);
+
+        return ways[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
-        int f[] = new int[n + 1];
-        System.out.println(fibonacci(n, f));
-        System.out.println(fibonacciTabulation(n));
+        // int f[] = new int[n + 1];
+        // System.out.println(fibonacci(n, f));
+        // System.out.println(fibonacciTabulation(n));
+        int ways[] = new int[n + 1];
+        Arrays.fill(ways, -1);
+        System.out.println(countWays(n, ways));
     }
 
 }
