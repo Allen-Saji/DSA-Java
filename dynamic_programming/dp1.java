71fda6f6814f8e3da6f1395ce254561a8fe31c31
Allen Saji <allensaji04@gmail.com>
2023-05-15 19:40:43 +0530

    add 01 kanpsack tabulation method

--- dynamic_programming/dp1.java
+++ dynamic_programming/dp1.java
@@ -82,6 +82,33 @@ public class dp1 {
         }
     }
 
+    public static int zerOneKnapsackTab(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= W; j++) {
+                int v = val[i - 1];// ith item value
+                int w = wt[i - 1];// ith item weight
+                if (w <= j) {
+                    int incProfit = v + dp[i - 1][j - w];// include profit
+                    int excProfit = dp[i - 1][j];// exlude profit
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                } else {
+                    int excProfit = dp[i - 1][j];// exlude profit
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+        return dp[n][W];
+    }
+
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n + 1];
@@ -94,13 +121,14 @@ public class dp1 {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
         int W = 7;
-        int dp[][] = new int[val.length + 1][W + 1];
-        for (int i = 0; i < (val.length + 1); i++) {
-            for (int j = 0; j < (W + 1); j++) {
-                dp[i][j] = -1;
-            }
-        }
-        System.out.println(zeroOneKnapsack(wt, val, W, val.length, dp));
+        // int dp[][] = new int[val.length + 1][W + 1];
+        // for (int i = 0; i < (val.length + 1); i++) {
+        // for (int j = 0; j < (W + 1); j++) {
+        // dp[i][j] = -1;
+        // }
+        // }
+        // System.out.println(zeroOneKnapsack(wt, val, W, val.length, dp));
+        System.out.println(zerOneKnapsackTab(val, wt, W));
     }
 
 }
