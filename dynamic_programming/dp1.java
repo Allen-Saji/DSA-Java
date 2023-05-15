e3acf3f719bdfe87efb47d0f2e8cb118f21f5edc
Allen Saji <allensaji04@gmail.com>
2023-05-15 06:46:08 +0530

    add o1 knapsack memoization

--- dynamic_programming/dp1.java
+++ dynamic_programming/dp1.java
@@ -61,19 +61,24 @@ public class dp1 {
         return dp[n];
     }
 
-    public static int zeroOneKnapsack(int wt[], int val[], int W, int n) {
+    public static int zeroOneKnapsack(int wt[], int val[], int W, int n, int dp[][]) {
         if (W == 0 || n == 0) {
             return 0;
         }
 
+        if (dp[n][W] != -1) {
+            return dp[n][W];
+        }
+
         if (wt[n - 1] <= W) {// valid
             // include
-            int ans1 = val[n - 1] + zeroOneKnapsack(wt, val, W - wt[n - 1], n - 1);
+            int ans1 = val[n - 1] + zeroOneKnapsack(wt, val, W - wt[n - 1], n - 1, dp);
             // exclude
-            int ans2 = zeroOneKnapsack(wt, val, W, n - 1);
-            return Math.max(ans1, ans2);
+            int ans2 = zeroOneKnapsack(wt, val, W, n - 1, dp);
+            dp[n][W] = Math.max(ans1, ans2);
+            return dp[n][W];
         } else {// not valid
-            return zeroOneKnapsack(wt, val, W, n - 1);
+            return zeroOneKnapsack(wt, val, W, n - 1, dp);
         }
     }
 
@@ -89,7 +94,13 @@ public class dp1 {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
         int W = 7;
-        System.out.println(zeroOneKnapsack(wt, val, W, val.length));
+        int dp[][] = new int[val.length + 1][W + 1];
+        for (int i = 0; i < (val.length + 1); i++) {
+            for (int j = 0; j < (W + 1); j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(zeroOneKnapsack(wt, val, W, val.length, dp));
     }
 
 }
