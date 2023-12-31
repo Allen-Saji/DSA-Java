39a6c6196ae9f85bb39d18de955dde68720efb93
Allen Saji <allensaji04@gmail.com>
2023-12-31 14:37:05 +0530

    add lcs using tabulation

--- dynamic_programming/Lcs.java
+++ dynamic_programming/Lcs.java
@@ -19,21 +19,45 @@ public class Lcs {
         }
     }
 
+    // lcs using tabulation
+    public static int lcs2(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n + 1][m + 1];
+        // java mein already initialization mein saare elemnts 0 hote hain, so need do
+        // the init case
+        // table filling
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
     public static void main(String args[]) {
         String str1 = "abcdefg";
-        String str2 = "adec";
+        String str2 = "adef";
         int n = str1.length();
         int m = str2.length();
         int dp[][] = new int[n + 1][m + 1];
         // for (int[] row : dp) {
         // Arrays.fill(row, -1);
         // }
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        System.out.println(lcs1(str1, str2, n, m, dp));
+        // for (int i = 0; i < n + 1; i++) {
+        // for (int j = 0; j < m + 1; j++) {
+        // dp[i][j] = -1;
+        // }
+        // }
+        // System.out.println(lcs1(str1, str2, n, m, dp));
+        System.out.println(lcs2(str1, str2));
 
     }
 }
