6151f96a97e5a97d66d5fbfb26e0c50a1a31f990
Allen Saji <allensaji04@gmail.com>
2024-01-21 12:44:41 +0530

    add mountain ranges problem

--- dynamic_programming/CatalanNumbers.java
+++ dynamic_programming/CatalanNumbers.java
@@ -54,6 +54,23 @@ public class CatalanNumbers {
         return dp[n];
     }
 
+    // same as catalan numbers
+    public static int mountainRanges(int n) {
+        int dp[] = new int[n + 1];
+        // initialization
+        dp[0] = dp[1] = 1;
+
+        // bottom up
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                int inside = dp[j];
+                int outside = dp[i - j - 1];
+                dp[i] += inside * outside;
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 4;
         int dp[] = new int[n + 1];
@@ -61,5 +78,6 @@ public class CatalanNumbers {
         System.out.println(catalanMem(n, dp));
         System.out.println(catalanTab(n));
         System.out.println(countBSTs(n));
+        System.out.println(mountainRanges(n));
     }
 }
